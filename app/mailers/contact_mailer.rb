f37df2a5796e5867912c474085740ee74235812d
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-03-19 11:35:11 -0500

    editing mailer

--- app/mailers/contact_mailer.rb
+++ app/mailers/contact_mailer.rb
@@ -2,7 +2,7 @@ class ContactMailer < ActionMailer::Base
 
   def contact_notification(contact)
   	@contact = contact
-  	mail(to: "aaronjohnsonis@gmail.com", subject: "Contact Notification", from: contact.email)
+  	mail(to: "binginlondon@gmail.com", subject: "Contact Notification", from: contact.email)
   end
 
 end
