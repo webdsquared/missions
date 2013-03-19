75fb310c9a383b78a5c0d32fe5713c540328228d
Aaron Johnson <aaronjohnsonis@gmail.com>
2013-03-18 20:45:25 -0500

    add database and fix mysql syntax

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -16,7 +16,7 @@ class QuestionsController < ApplicationController
   	@questions = Question.order("position ASC")
   	@blog_list = Blog.order("name asc")
     @latest_posts = Post.order("published_on desc").limit(4)
-    @side_posts = Post.where('status == ? AND published_on <= ?', "Published", Date.today).order("published_on desc").limit(10)
+    @side_posts = Post.where('status = ? AND published_on <= ?', "Published", Date.today).order("published_on desc").limit(10)
     @all_categories = Category.order("name ASC")
   end
 end
